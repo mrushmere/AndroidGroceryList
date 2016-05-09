e84e040c37ca093da32fafaa8f72f6aea86c6b48
Mark Rushmere <Mark Rushmere>
2016-05-09 17:11:03 -0400

    list view for get request working

--- app/src/main/java/com/markrushmere/grocerylist/GetActivity.java
+++ app/src/main/java/com/markrushmere/grocerylist/GetActivity.java
@@ -42,68 +42,81 @@ import okhttp3.Response;
 
 
 public class GetActivity extends AppCompatActivity {
+    ListView listView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get);
 
+        listView = (ListView)findViewById(R.id.list_view2);
+        listView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,new ArrayList<String>()));
 
-        new AsyncTask<Void, Void, String>() {
-            @Override
-            protected String doInBackground(Void ... Params) {
-                OkHttpClient client = new OkHttpClient();
-                Request request = new Request.Builder()
-                        .url("https://api2.bigoven.com/grocerylist?api_key=lkEGrI9g8gXCM9P2AGxR1OeT94L3SEj8")
-                        .header("type", "GET")
-                        .addHeader("dataType", "json")
-                        .addHeader("Authorization", "Basic bWFyazY3OTA6dGVkZHlyMDE=")
-                        .build();
+        new MyTask().execute();
 
-                try {
-                    Response response = client.newCall(request).execute();
-                    String jsonData = response.body().string();
-                    try {
-                        JSONObject Jobject = new JSONObject(jsonData);
-                        JSONArray Jarray = Jobject.getJSONArray("Items");
-                        List<String> groceryListGet = new ArrayList<String>(Jarray.length());
-                        for (int i = 0; i < Jarray.length(); i++) {
-                            JSONObject object     = Jarray.getJSONObject(i);
-                            String listItem = object.getString("Name");
-                            groceryListGet.add(listItem);
-                            Collections.sort(groceryListGet);
-                            Log.d("ITEMS", object.toString());
-                        }
-                    } catch (JSONException e) {
-                        throw new RuntimeException(e);
-                    }
-                    Log.d("RES", response.body().string());
-                    return response.body().string();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return null;
-            }
-        }.execute();
 
-        /*****************
-        String jsonData = response.body().string();
-        JSONObject Jobject = new JSONObject(jsonData);
-        JSONArray Jarray = Jobject.getJSONArray("Items");
-        List<String> groceryListGet = new ArrayList<String>(Jarray.length());
-        for (int i = 0; i < Jarray.length(); i++) {
-            JSONObject object     = Jarray.getJSONObject(i);
-            String listItem = object.getString("Name");
-            groceryListGet.add(listItem);
-            Collections.sort(groceryListGet);
-            Log.d("ITEMS", object.toString());
+    }
+
+    class MyTask extends AsyncTask<Void, String, String> {
+
+        ArrayAdapter<String> adapter;
+        @Override
+        protected void onPreExecute() {
+            adapter = (ArrayAdapter<String>)listView.getAdapter();
+
         }
 
-        ListView myListView = (ListView) findViewById(R.id.list_view2);
-        myListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, groceryListGet));
-        ************************/
+        @Override
+        protected String doInBackground(Void... params) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url("https://api2.bigoven.com/grocerylist?api_key=lkEGrI9g8gXCM9P2AGxR1OeT94L3SEj8")
+                    .header("type", "GET")
+                    .addHeader("dataType", "json")
+                    .addHeader("Authorization", "Basic bWFyazY3OTA6dGVkZHlyMDE=")
+                    .build();
 
+            try {
+                Response response = client.newCall(request).execute();
+                String jsonData = response.body().string();
+                try {
+                    JSONObject Jobject = new JSONObject(jsonData);
+                    JSONArray Jarray = Jobject.getJSONArray("Items");
+                    List<String> groceryListGet = new ArrayList<String>(Jarray.length());
+                    for (int i = 0; i < Jarray.length(); i++) {
+                        JSONObject object     = Jarray.getJSONObject(i);
+                        String listItem = object.getString("Name");
+                        publishProgress(listItem);
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        Log.d("ITEMS",listItem);
+                    }
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+                Log.d("RES", response.body().string());
 
+                return response.body().string();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return "All the list item were received";
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            adapter.add(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+        }
     }
 }
--- app/src/main/java/com/markrushmere/grocerylist/ListActivity.java
+++ app/src/main/java/com/markrushmere/grocerylist/ListActivity.java
@@ -118,7 +118,7 @@ public class ListActivity extends AppCompatActivity {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     // Send the request to the API to Sync the list
-
+                    
 
                 }
             });
